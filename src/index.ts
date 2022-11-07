eee4df1082984ddc403076efe8db91a762fda6ab
Shlok D. Mange <shlok_mange@protonmail.com>
2022-11-07 08:20:03 +0530

    init express app

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import { config } from 'dotenv'
+import { urlencoded } from 'body-parser'
+config()
+
+const app = express()
+const port = process.env.PORT || 8080
+
+//middleware
+app.use(express.json())
+app.use(urlencoded({ extended: false }))
+
+app.listen(port, () => {
+    console.log(`Bytekode API is live on port: ${port}`)
+})
